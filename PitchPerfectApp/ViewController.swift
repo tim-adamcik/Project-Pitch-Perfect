5f97f66232df6077ea0fced4c5dd713d0717fc34
Timothy Adamcik <tadamcik0425@gmail.com>
2019-10-06 17:16:07 -0400

    added buttons labels and auto layout constraints

--- PitchPerfectApp/ViewController.swift
+++ PitchPerfectApp/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("stop button was pressed")
+    }
 }
 
