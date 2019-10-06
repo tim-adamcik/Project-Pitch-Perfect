6239f66eb2e5832684a1ca1b4a43ab037c4fecee
Timothy Adamcik <tadamcik0425@gmail.com>
2019-10-06 17:36:03 -0400

    enabled and disabled buttons

--- PitchPerfectApp/ViewController.swift
+++ PitchPerfectApp/ViewController.swift
@@ -13,20 +13,30 @@ import AVFoundation
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stopRecordingButton.isEnabled = false
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+         super.viewWillAppear(animated)
+         print("viewWillAppear Called")
+     }
+    
     @IBAction func recordAudio(_ sender: Any) {
-        print("record button was pressed")
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
     
     @IBAction func stopRecording(_ sender: Any) {
-        print("stop button was pressed")
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
     }
 }
 
